cf9ad307a05a2517c9bc30278ecbaa0be2064a6d
Djamel <djameldev@gmail.com>
2023-08-06 18:40:58 +0200

    create book sans image

---
+++ backend/lib/jwt.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET = "djamel-secret";
+
+const verify = (token) => jwt.verify(token, SECRET);
+
+const getToken = (user) =>
+  jwt.sign({ userId: user._id }, SECRET, {
+    expiresIn: "1d",
+  });
+
+module.exports = {
+  verify,
+  getToken,
+};
---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+const { verify } = require("../lib/jwt")
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    const decodedToken = verify(token, process.env.TOKEN)
+    const userId = decodedToken.userId
+    req.auth = {
+      userId: userId,
+    }
+    next()
+  } catch (error) {
+    res.status(403).json({ error })
+  }
+}
